e739387e939d9de7a852bda9c60d6e230cb1c88c
dave <dub.cyclone@gmail.com>
2016-07-01 01:56:05 +0100

    + config & machine

--- src/ED_Console/Program.cs
+++ src/ED_Console/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NetProcgame;
+using NetProcgame.Game;
+using NetProcgame.Logging;
 
 namespace ED_Console
 {
@@ -10,6 +13,8 @@ namespace ED_Console
     {
         static void Main(string[] args)
         {
+            HorseGame game = new HorseGame(NetProcgame.NetPinproc.MachineType.WPC95, new ConsoleLogger());
+
         }
     }
 }
